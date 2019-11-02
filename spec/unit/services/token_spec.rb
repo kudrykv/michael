3ee28113ab9452f820cb6a66439f8f7890a6d7d9
Vitaliy Kudryk <kudryk.v@gmail.com>
2019-11-02 22:01:13 +0200

    upd and fix test

--- spec/unit/services/token_spec.rb
+++ spec/unit/services/token_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'michael/services/token'
+require 'michael/services/github/token'
 
-RSpec.describe Michael::Services::Token do
+RSpec.describe Michael::Services::Github::Token do
   let(:octokit) { double(:octokit) }
   let(:exception) { Michael::Error }
   let(:config) { double(:config) }
@@ -15,7 +15,7 @@ RSpec.describe Michael::Services::Token do
       expect(octokit).to receive(:new).with(access_token: :access_token).and_return(octokit)
       expect(octokit).to receive(:scopes).and_return(['repo'])
 
-      tkn = Michael::Services::Token.new(config)
+      tkn = Michael::Services::Github::Token.new(config)
 
       expect { tkn.validate(:access_token) }.not_to raise_error
     end
@@ -27,7 +27,7 @@ RSpec.describe Michael::Services::Token do
       expect(octokit).to receive(:new).with(access_token: :access_token).and_return(octokit)
       expect(octokit).to receive(:scopes).and_raise(exception, 'not a token')
 
-      tkn = Michael::Services::Token.new(config)
+      tkn = Michael::Services::Github::Token.new(config)
 
       expect { tkn.validate(:access_token) }.to raise_error(Michael::Error, 'invalid access token')
     end
@@ -38,7 +38,7 @@ RSpec.describe Michael::Services::Token do
       expect(octokit).to receive(:new).with(access_token: :access_token).and_return(octokit)
       expect(octokit).to receive(:scopes).and_return(['no', 'nopes', 'not here'])
 
-      tkn = Michael::Services::Token.new(config)
+      tkn = Michael::Services::Github::Token.new(config)
       expect { tkn.validate(:access_token) }
         .to raise_error(Michael::Error, 'access token must contain `repo` scope')
     end
@@ -48,9 +48,19 @@ RSpec.describe Michael::Services::Token do
     it 'should store the token' do
       expect(config).to receive(:set).with(:token, value: access_token)
 
-      tkn = Michael::Services::Token.new(config)
+      tkn = Michael::Services::Github::Token.new(config)
 
       expect { tkn.store(access_token) }.not_to raise_error
+    end
+  end
+
+  context 'fetch' do
+    it 'should should read tkn when it is there' do
+      expect(config).to receive(:fetch).with(:token).and_return(access_token)
+
+      tkn = Michael::Services::Github::Token.new(config)
+
+      expect(tkn.token).to eq(access_token)
     end
   end
 end
