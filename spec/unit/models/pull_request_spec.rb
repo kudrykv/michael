8b356f6bdbf9e95dc2aa4c710bda665fa97c0f07
Vitaliy Kudryk <kudryk.v@gmail.com>
2019-11-05 22:47:00 +0200

    start testing

--- spec/unit/models/pull_request_spec.rb
+++ spec/unit/models/pull_request_spec.rb
@@ -3,26 +3,59 @@
 require_relative '../../../lib/michael/models/pull_request'
 
 RSpec.describe Michael::Models::PullRequest do
+  Label = Struct.new(:name)
+
   let(:pr_number) { 42 }
   let(:sha) { 'sha' }
+  let(:author) { 'deadbeef' }
+  let(:title) { 'that is a great title' }
+  let(:labels) { ['shiny', 'label'] }
+
   let(:raw_pr) do
     {
       number: pr_number,
+      title: title,
+      user: {
+        login: author
+      },
       head: {
         sha: sha
-      }
+      },
+      labels: [Label.new('shiny'), Label.new('label')]
     }
   end
 
-  context 'pull request' do
-    it 'should return number' do
-      pr = Michael::Models::PullRequest.new(raw_pr)
-      expect(pr.number).to be(pr_number)
+  let(:pr) { Michael::Models::PullRequest.new(raw_pr) }
+
+  context 'simple methods' do
+    context :number do
+      it 'should return a number' do
+        expect(pr.number).to eq(pr_number)
+      end
     end
 
-    it 'should should return sha number' do
-      pr = Michael::Models::PullRequest.new(raw_pr)
-      expect(pr.head_sha).to be(sha)
+    context :title do
+      it 'should return title' do
+        expect(pr.title).to eq(title)
+      end
+    end
+
+    context :author do
+      it 'should return an author' do
+        expect(pr.author).to eq(author)
+      end
+    end
+
+    context :head_sha do
+      it 'should should return sha number' do
+        expect(pr.head_sha).to eq(sha)
+      end
+    end
+
+    context :labels do
+      it 'should return list of labels' do
+        expect(pr.labels).to contain_exactly(*labels)
+      end
     end
   end
 end
